9a68e5bfaf10833d390eb26d47165553658814be
imeujin <imeujin.dev@gmail.com>
2024-12-12 18:39:22 +0900

    design: 채팅방 bottomsheet top padding 값 추가

--- lib/ui/pages/chat_room/widgets/chat_room_bottomsheet.dart
+++ lib/ui/pages/chat_room/widgets/chat_room_bottomsheet.dart
@@ -12,6 +12,7 @@ class ChatRoomBottomsheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(
+        top: 10,
         left: 20,
         right: 20,
         bottom: bottomPaddingSize,
