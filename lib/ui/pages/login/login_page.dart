2ad29536880bb2bb6f9f1b649034671b50bb44d4
ParkChaeEun08 <pce980810@gmail.com>
2024-12-12 20:16:31 +0900

    feat : login page 및 snackbar 추가

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,8 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:korea_pet_help_diary/ui/pages/home/home_page.dart';
+import 'package:korea_pet_help_diary/ui/pages/join/join_page.dart';
+import 'package:korea_pet_help_diary/ui/pages/theme/theme.dart';
+import 'package:korea_pet_help_diary/util/snackbar.dart';
 
-class LoginPage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text('LoginPage');
+    return MaterialApp(
+      theme: appTheme,
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('로그인'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/welcome.png'),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: '이메일',
+              ),
+            ),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: '비밀번호',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                bool loginSuccess = true;
+
+                if (loginSuccess) {
+                  showCustomSnackBar(context, '로그인 성공');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => HomePage()), // 로그인 성공 시 홈 페이지로 이동
+                  );
+                } else {
+                  showCustomSnackBar(context, '로그인 실패');
+                }
+              },
+              child: const Text('로그인'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => JoinPage()), // 회원가입 페이지로 이동
+                );
+              },
+              child: const Text('계정이 없다면? 회원가입'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
