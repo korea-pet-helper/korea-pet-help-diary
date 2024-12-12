569bdab95a0c390c981ceea6820631f9ba286aa3
imeujin <imeujin.dev@gmail.com>
2024-12-12 16:59:48 +0900

    feat: 채팅 눌렀을 때 채팅방으로 이동하는 기능 구현

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:korea_pet_help_diary/ui/pages/chat_room/chat_room_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -31,63 +32,72 @@ class HomePage extends StatelessWidget {
   }
 
   Widget item() {
-    return GestureDetector(
-      onTap: () {
-        // TODO: chat_room 페이지로 이동
-        print('container 누름');
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        height: 100,
-        decoration: BoxDecoration(
-          color: Colors.transparent,
-          border: Border(
-            bottom: BorderSide(color: Colors.grey[400]!),
+    return Builder(builder: (context) {
+      return GestureDetector(
+        onTap: () {
+          // TODO: chat_room 페이지로 이동
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return ChatRoomPage();
+              },
+            ),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            border: Border(
+              bottom: BorderSide(color: Colors.grey[400]!),
+            ),
+          ),
+          child: Row(
+            children: [
+              // TODO: 사진 뭐로?
+              ClipOval(
+                child: Image.network(
+                  'https://picsum.photos/200/300',
+                  height: 60,
+                  width: 60,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '숭의동',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Spacer(),
+                        Text(
+                          '30분 전',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '안녕하세요. 반갑습니다.',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-        child: Row(
-          children: [
-            // TODO: 사진 뭐로?
-            ClipOval(
-              child: Image.network(
-                'https://picsum.photos/200/300',
-                height: 60,
-                width: 60,
-                fit: BoxFit.fill,
-              ),
-            ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        '숭의동',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Spacer(),
-                      Text(
-                        '30분 전',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-                  Text(
-                    '안녕하세요. 반갑습니다.',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black54,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+      );
+    });
   }
 }
