0b188172cc93d6a73c9ec33a4f2387c33644ffec
imeujin <imeujin.dev@gmail.com>
2024-12-12 16:49:52 +0900

    design: 채팅방 메세지 보내는 bottomsheet 구현

---
+++ lib/ui/pages/chat_room/widgets/chat_room_bottomsheet.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ChatRoomBottomsheet extends StatelessWidget {
+  // 2a52be
+  static const ceruleanBlue = Color.fromARGB(255, 42, 82, 190);
+
+  TextEditingController controller;
+  double bottomPaddingSize;
+  ChatRoomBottomsheet(this.controller, this.bottomPaddingSize);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        left: 20,
+        right: 20,
+        bottom: bottomPaddingSize,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: controller,
+              textInputAction: TextInputAction.send,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          GestureDetector(
+            onTap: () {
+              // 메시지 전달
+              print('메시지 전달');
+            },
+            child: Container(
+              height: 50,
+              width: 50,
+              color: Colors.transparent,
+              child: const Icon(
+                Icons.send,
+                size: 30,
+                color: ceruleanBlue,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
