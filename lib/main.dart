1672e713beb115f3a3165799112a49f2a7c228d3
imeujin <imeujin.dev@gmail.com>
2024-12-13 11:17:35 +0900

    add: firebase firestore 추가 및 riverpod 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:korea_pet_help_diary/firebase_options.dart';
 import 'package:korea_pet_help_diary/ui/pages/theme/theme.dart';
 import 'package:korea_pet_help_diary/ui/pages/login/login_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // runApp 실행 전 비동기 함수 사용해서 데이터 초기화할 때 사용
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // firebase 설정
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // ProviderScope 추가
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
