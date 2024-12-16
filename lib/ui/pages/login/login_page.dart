256d00ece65d371309e8e3e7f6bb16604bdf3f36
ParkChaeEun08 <pce980810@gmail.com>
2024-12-16 14:36:23 +0900

    comment : 주석 정리

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -4,8 +4,7 @@ import 'package:korea_pet_help_diary/ui/pages/home/home_page.dart';
 import 'package:korea_pet_help_diary/ui/pages/join/join_page.dart';
 import 'package:korea_pet_help_diary/util/snackbar.dart';
 
-//로그인 후 홈페이지로 로컬코드도 넘겨주기
-
+// 로그인 페이지 클래스
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
   @override
@@ -13,10 +12,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // 텍스트 컨트롤러 선언
   final TextEditingController idController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool isButtonEnabled = false; // 로그인 버튼 활성화 상태
+
   // 아이디와 비밀번호 입력 상태에 따라 버튼 활성화 상태를 업데이트하는 메서드
   void _updateButtonState() {
     setState(() {
@@ -42,6 +43,7 @@ class _LoginPageState extends State<LoginPage> {
           // 로컬코드 가져오기
           String localCode = userDoc['localCode'];
 
+          // 홈 페이지로 이동하면서 로컬코드 전달
           Navigator.push(
             context,
             MaterialPageRoute(
@@ -87,12 +89,14 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              // 로고 이미지
               Image.asset(
                 'assets/images/welcome.png',
                 height: 150,
                 width: 150,
               ),
               const SizedBox(height: 20),
+              // 아이디 입력 필드
               TextFormField(
                 controller: idController,
                 decoration: const InputDecoration(
@@ -105,6 +109,7 @@ class _LoginPageState extends State<LoginPage> {
                   return null;
                 },
               ),
+              // 비밀번호 입력 필드
               TextFormField(
                 controller: passwordController,
                 obscureText: true,
@@ -119,11 +124,13 @@ class _LoginPageState extends State<LoginPage> {
                 },
               ),
               const SizedBox(height: 20),
+              // 로그인 버튼
               ElevatedButton(
                 onPressed:
                     isButtonEnabled ? _loginUser : null, // 버튼 활성화 상태에 따라 로그인 시도
                 child: const Text('로그인'),
               ),
+              // 회원가입 페이지로 이동 버튼
               TextButton(
                 onPressed: () {
                   Navigator.push(
