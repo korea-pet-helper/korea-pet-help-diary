3cfc7fe25a625a09cd8e9a5e91c45f6cec867b46
imeujin <imeujin.dev@gmail.com>
2024-12-16 20:22:14 +0900

    fix: 주석 처리 된 코드 제거

--- lib/ui/pages/chat_room/widgets/chat_room_receive.dart
+++ lib/ui/pages/chat_room/widgets/chat_room_receive.dart
@@ -76,21 +76,3 @@ class ChatRoomReceive extends StatelessWidget {
     );
   }
 }
-
-// Container(
-//                 // TODO: 메시지 길이에 따라 container 사이즈 조절 필요
-//                 height: 80,
-//                 width: 280,
-//                 decoration: BoxDecoration(
-//                   border: Border.all(color: Colors.grey),
-//                   borderRadius: BorderRadius.circular(8),
-//                 ),
-//                 padding: const EdgeInsets.all(8),
-//                 child: Text(
-//                   softWrap: true,
-//                   chat.message,
-//                   style: TextStyle(
-//                     fontSize: 18,
-//                   ),
-//                 ),
-//               ),
