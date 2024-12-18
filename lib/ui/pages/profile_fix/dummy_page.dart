2eb50afc9839588dc1560a431bbf2275f7562a7b
Jeongwoo-KGI <jeongwoo@uni.minerva.edu>
2024-12-18 09:29:58 +0900

    fix: repositioning main box

--- lib/ui/pages/profile_fix/dummy_page.dart
+++ lib/ui/pages/profile_fix/dummy_page.dart
@@ -34,130 +34,138 @@ class _DummyPageState extends State<DummyPage> {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 40, bottom: 20),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  child: Stack(
-                    children: [
-                      Image.asset('assets/images/welcome.png', height: 60,width: 60,),
-                      Positioned(
-                        top: 0,
-                        right: 0,
-                        child: Icon(Icons.camera),
-                      ),
-                    ],
-                  ),
-                ), 
-
-              ),
-            
-          
-              SizedBox(
-                height: 600,
-                width: 440,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Text(
-                      style: TextStyle(fontSize: 12),
-                      '닉네임',
-                    ),
-                    //textformfield alternative
-                    SizedBox(height: 44, width: double.infinity,),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 22.5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, bottom: 20,),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    child: Stack(
                       children: [
-                        ToggleButtons(
-                          color: Colors.black.withOpacity(0.6),
-                          selectedColor: Colors.white,
-                          fillColor: Colors.black,
-                          splashColor: Colors.black,
-                          borderRadius: BorderRadius.circular(8.0),
-                          isSelected: isSelected,
-                          onPressed:toggleSelect,
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.symmetric(horizontal:10),
-                              child: const SizedBox(
-                                height:double.infinity,
-                                child: Text("개", style: TextStyle(fontSize: 12)),),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(horizontal:10),
-                              child: const SizedBox(
-                                height:double.infinity,
-                                child: Text("고양이", style: TextStyle(fontSize: 12)),),
-                            ),
-                          ],
+                        Image.asset('assets/images/welcome.png', height: 60,width: 60,),
+                        Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Icon(Icons.camera),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 30),
-                    const Text(
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 12,
-                      ),
-                      '반려동물 성격',
-                    ),
-                    //alternative to textformfield
-                    SizedBox(height: 44, width: double.infinity,),
-                    const SizedBox(height: 5,),
-                    const Text(
-                      '내 동네',
-                      style: TextStyle(
-                        fontSize: 12
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: SizedBox(
-                        height: 50,
-                        width: 400,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                  ), 
+            
+                ),
+            
+                Container(
+                  height: 500,
+                  width: 350,
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          style: TextStyle(fontSize: 12),
+                          '닉네임',
+                        ),
+                        //textformfield alternative
+                        SizedBox(height: 44, width: double.infinity,),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            const SizedBox(
-                              height: 50,
-                              width: 50,
-                              child: Icon(Icons.location_on_outlined),
-                            ),
-                            Text(
-                              "서울시청",
-                              style: const TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w200,
-                              ),
+                            ToggleButtons(
+                              color: Colors.black.withOpacity(0.6),
+                              selectedColor: Colors.white,
+                              fillColor: Colors.black,
+                              splashColor: Colors.black,
+                              borderRadius: BorderRadius.circular(8.0),
+                              isSelected: isSelected,
+                              onPressed:toggleSelect,
+                              children: [
+                                Container(
+                                  margin: const EdgeInsets.symmetric(horizontal:10),
+                                  child: const SizedBox(
+                                    height:double.infinity,
+                                    child: Text("개", style: TextStyle(fontSize: 12)),),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.symmetric(horizontal:10),
+                                  child: const SizedBox(
+                                    height:double.infinity,
+                                    child: Text("고양이", style: TextStyle(fontSize: 12)),),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ),
+                        const SizedBox(height: 30),
+                        const Text(
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 12,
+                          ),
+                          '반려동물 성격',
+                        ),
+                        //alternative to textformfield
+                        SizedBox(height: 44, width: double.infinity,),
+                        const SizedBox(height: 5,),
+                        const Text(
+                          '내 동네',
+                          style: TextStyle(
+                            fontSize: 12
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            
+                          },
+                          child: const SizedBox(
+                            height: 50,
+                            width: 400,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                  width: 50,
+                                  child: Icon(Icons.location_on_outlined),
+                                ),
+                                Text(
+                                  "서울시청",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w200,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 40,),
-              Container(
-                height: 50,
-                width: 250,
-                child: ElevatedButton(
-                  onPressed: (){},
-                  child: const SizedBox(
-                    height: double.infinity,
-                    width: double.infinity,
-                    child: Text("수정하기"),
-                  ), 
+                const SizedBox(height: 40,),
+                Container(
+                  height: 50,
+                  width: 250,
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    child: const SizedBox(
+                      height: double.infinity,
+                      width: double.infinity,
+                      child: Text("수정하기"),
+                    ), 
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/ui/pages/profile_fix/profile_fix_page.dart
+++ lib/ui/pages/profile_fix/profile_fix_page.dart
@@ -81,175 +81,193 @@ class _ProfileFixPageState extends ConsumerState<ProfileFixPage> {
           key: _formKey,
           child: SafeArea(
             child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //profile image
-                  Padding(
-                    padding: const EdgeInsets.only(top: 40, bottom: 40),
-                    
-                    child: Container(
-                      //image file with overlapping icon
-                      height: 80,
-                      width: 80,
-                    ),
-                  ),
-                  //box with information
-                  SizedBox(
-                    height: 600,
-                    width: 440,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const Text(
-                          style: TextStyle(
-                            fontSize: 12,
-                          ),
-                          '닉네임',
-                        ),
-                        TextFormField(
-                          controller: nickNameController,
-                          textInputAction: TextInputAction.done,
-                          decoration: const InputDecoration(hintText: '바둑이'),
-                          validator: (value){
-                            if (value?.trim().isEmpty ?? true) {
-                              return '닉네임을 입력하세요';
-                            }
-                            return null;
-                          },
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 22.5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //profile image
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40, bottom: 40),
+                      
+                      child: Container(
+                        //image file with overlapping icon
+                        //ToDo: make it a gesture detector for the image
+                        height: 80,
+                        width: 80,
+                        child: Stack(
                           children: [
-                            ToggleButtons(
-                              color: Colors.black.withOpacity(0.6),
-                              selectedColor: Colors.white,
-                              fillColor: Colors.black,
-                              splashColor: Colors.black,
-                              borderRadius: BorderRadius.circular(8.0),
-                              isSelected: isSelected,
-                              onPressed:toggleSelect,
-                              children: [
-                                Container(
-                                  margin: const EdgeInsets.symmetric(horizontal:10),
-                                  child: const SizedBox(
-                                    height:double.infinity,
-                                    child: Text("개", style: TextStyle(fontSize: 12)),),
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.symmetric(horizontal:10),
-                                  child: const SizedBox(
-                                    height:double.infinity,
-                                    child: Text("고양이", style: TextStyle(fontSize: 12)),),
-                                ),
-                              ],
+                            Image.asset('assets/images/welcome.png', height: 60,width: 60,),
+                            const Positioned(
+                              top: 0,
+                              right: 0,
+                              child: Icon(Icons.camera),
                             ),
-              
                           ],
                         ),
-                        const SizedBox(height: 30),
-                        const Text(
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 12,
-                          ),
-                          '반려동물 성격',
-                        ),
-                        TextFormField(
-                          controller: charactersController,
-                          textInputAction: TextInputAction.newline,
-                          maxLines: null,
-                          expands: true,
-                          decoration: const InputDecoration(hintText: '착하고 순해요'),
-                          validator: (value) {
-                            if(value?.trim().isEmpty ?? true) {
-                              return '반려동물의 성격을 하나 이상 입력해주세요';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(height: 5),
-                        const Text(
-                          '내 동네',
-                          style: TextStyle(
-                            fontSize: 12
-                          ),
-                        ),
-                        GestureDetector(
-                          
-                          onTap: () {
-                          },
-                          child: SizedBox(
-                            height: 50,
-                            width: 400,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
+                      ),
+                    ),
+                    //box with information
+                    SizedBox(
+                      height: 500,
+                      width: 350,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
+                              '닉네임',
+                            ),
+                            TextFormField(
+                              controller: nickNameController,
+                              textInputAction: TextInputAction.done,
+                              decoration: const InputDecoration(hintText: '바둑이'),
+                              validator: (value){
+                                if (value?.trim().isEmpty ?? true) {
+                                  return '닉네임을 입력하세요';
+                                }
+                                return null;
+                              },
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
-                                //making the icon to go to the left
-                                const SizedBox(height: 50,width: 50, child: Icon(Icons.location_on_outlined)),
-                                Text(
-                                  "서울시청",//location['name'], 
-                                  style: const TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w200,
-                                  ),),
+                                ToggleButtons(
+                                  color: Colors.black.withOpacity(0.6),
+                                  selectedColor: Colors.white,
+                                  fillColor: Colors.black,
+                                  splashColor: Colors.black,
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  isSelected: isSelected,
+                                  onPressed:toggleSelect,
+                                  children: [
+                                    Container(
+                                      margin: const EdgeInsets.symmetric(horizontal:10),
+                                      child: const SizedBox(
+                                        height:double.infinity,
+                                        child: Text("개", style: TextStyle(fontSize: 12)),),
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.symmetric(horizontal:10),
+                                      child: const SizedBox(
+                                        height:double.infinity,
+                                        child: Text("고양이", style: TextStyle(fontSize: 12)),),
+                                    ),
+                                  ],
+                                ),
+                                      
                               ],
                             ),
+                            const SizedBox(height: 30),
+                            const Text(
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                              ),
+                              '반려동물 성격',
+                            ),
+                            TextFormField(
+                              controller: charactersController,
+                              textInputAction: TextInputAction.newline,
+                              maxLines: null,
+                              expands: true,
+                              decoration: const InputDecoration(hintText: '착하고 순해요'),
+                              validator: (value) {
+                                if(value?.trim().isEmpty ?? true) {
+                                  return '반려동물의 성격을 하나 이상 입력해주세요';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(height: 5),
+                            const Text(
+                              '내 동네',
+                              style: TextStyle(
+                                fontSize: 12
+                              ),
+                            ),
+                            GestureDetector(
+                              
+                              onTap: () {
+                              },
+                              child: SizedBox(
+                                height: 50,
+                                width: 250,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    //making the icon to go to the left
+                                    const SizedBox(height: 50,width: 50, child: Icon(Icons.location_on_outlined)),
+                                    Text(
+                                      "서울시청",//location['name'], 
+                                      style: const TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w200,
+                                      ),),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            // TextFormField(
+                            //   controller: myNeighborController,
+                            //   textInputAction: TextInputAction.done,
+                            //   decoration: const InputDecoration(hintText: '서울시 용산구 한강대로 100'),
+                            //   validator: (value) {
+                            //     if(value?.trim().isEmpty ?? true) {
+                            //       return '지역을 입력해주세요';
+                            //     }
+                            //     return null;
+                            //   },
+                              //adding the icon to the right
+                            
+                          ],
                           ),
-                        ),
-                        // TextFormField(
-                        //   controller: myNeighborController,
-                        //   textInputAction: TextInputAction.done,
-                        //   decoration: const InputDecoration(hintText: '서울시 용산구 한강대로 100'),
-                        //   validator: (value) {
-                        //     if(value?.trim().isEmpty ?? true) {
-                        //       return '지역을 입력해주세요';
-                        //     }
-                        //     return null;
-                        //   },
-                          //adding the icon to the right
-                        
-                      ],
                       ),
-                  ),
-                  //empty gap
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Container(
-                    height: 50,
-                    width: 250,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        final result = _formKey.currentState?.validate() ?? false;
-                        if (result) {
-                          final vm = ref.read(profileFixViewModelProvider(widget.user).notifier);
-                          final resetResult = await vm.reset(
-                            //userId: '',
-                            imageUrl: '', //ToDo: needs fix
-                            local: "서울시청",
-                            localCode: "28260122", //ToDo: needs fix
-                            //password: '',
-                            nickname: nickNameController.text,
-                            //phone: '',
-                            pet: Pet(petInformation: charactersController.text),
-                          );
-                          if(resetResult) {
-                            Navigator.push(context, MaterialPageRoute(builder: (context){
-                              return HomePage(user: Sample);//this needs to be filled
-                            }));
-                          };
-                          }
-                      },
-                      child: const SizedBox(
-                        height: double.infinity,
-                        width: double.infinity,
-                        child: Text("수정하기")),
                     ),
-                  ),
-                ],   
-              
+                    //empty gap
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 250,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          final result = _formKey.currentState?.validate() ?? false;
+                          if (result) {
+                            final vm = ref.read(profileFixViewModelProvider(widget.user).notifier);
+                            final resetResult = await vm.reset(
+                              //userId: '',
+                              imageUrl: '', //ToDo: needs fix
+                              local: "서울시청",
+                              localCode: "28260122", //ToDo: needs fix
+                              //password: '',
+                              nickname: nickNameController.text,
+                              //phone: '',
+                              pet: Pet(petInformation: charactersController.text),
+                            );
+                            if(resetResult) {
+                              Navigator.push(context, MaterialPageRoute(builder: (context){
+                                return HomePage(user: Sample);//this needs to be filled
+                              }));
+                            };
+                            }
+                        },
+                        child: const SizedBox(
+                          height: double.infinity,
+                          width: double.infinity,
+                          child: Text("수정하기")),
+                      ),
+                    ),
+                  ],   
+                
+                ),
               ),
             ),
           ),
